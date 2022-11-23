1c38b7b5e811df4b6ad95ae4a454105b4737229f
Emre Taskale <110481472+EM-T-Shells@users.noreply.github.com>
2022-11-22 23:57:23 -0600

    add files via upload
    authentication permission denied by github.

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const teamMember_id = document.getElementById("teamMember_id")
+const teamMember_position = document.getElementById("teamMember_position");
+const teamMember_email = document.getElementById("teamMember_email")
+const teamMember_place = document.getElementById("teamMember_place");
